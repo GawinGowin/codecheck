b10023515e2a4dc218bf1d1d828bc7fea7771c2e
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2023-05-06 02:26:02 +0000

    change sample code

--- src/main.py
+++ src/main.py
@@ -1,29 +1,8 @@
 def main():
-  _ = int(input())
-  S = [int(i) for i in input().split(" ")]
-  _ = int(input())
-  T = [int(i) for i in input().split(" ")]
-
-  count = 0
-  for t in T:
-    left = S[0]
-    right = S[-1]
-    if (t > S[-1]) | (t < S[0]):
-      break
-
-    while right >= left:
-      s = (right + left) // 2
-      if t == s:
-        count += 1
-        break
-      elif t < s:
-        right = s
-
-      elif t > s:
-        left = s
-
-  print(count)
-
+    a = int(input())
+    b, c = map(int, input().split())
+    s = input()
+    print("{} {}".format(a+b+c, s))
 
 if __name__ == '__main__':
-  main()
+    main()
--- src/test_main.py
+++ src/test_main.py
@@ -2,15 +2,15 @@ from main import *
 import pytest
 import io
 
-@pytest.mark.parametrize(('fileNo'), [1, 2, 3])
+@pytest.mark.parametrize(('fileNo'), [1, 2,])
 def test_case(capsys, monkeypatch, fileNo):
-    inputs, outputs = loadData(fileNo)
+    inputs, outputs = load_data(fileNo)
     monkeypatch.setattr('sys.stdin', io.StringIO(inputs))
     main()
     results, _ = capsys.readouterr()
     assert results == outputs + "\n"
 
-def loadData(fileNo):
+def load_data(fileNo):
     inputFile = F"src/input/input{fileNo}.txt"
     outputFile = F"src/output/output{fileNo}.txt"
     with open(inputFile, "r") as f:
